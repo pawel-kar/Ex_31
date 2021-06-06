a13e0feae0263e685d62d269bebf26b2508ffc53
Pawel Karpinski <karpinsk.pawel@gmail.com>
2021-06-06 18:23:55 +0200

    Solution_31

---
+++ src/main/java/pl/pelikan/weatherinthecity/WeatherinthecityApplication.java
@@ -0,0 +1,13 @@
+package pl.pelikan.weatherinthecity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherinthecityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherinthecityApplication.class, args);
+    }
+
+}
